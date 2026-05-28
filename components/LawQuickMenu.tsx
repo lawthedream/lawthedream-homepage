2d14c9ab19de768802e5032634d046cecc8dd2f2
lawthedream1101 <cpla9999@gmail.com>
2026-05-28 08:29:01 +0000

    Fix3: 퀵메뉴 quick_01 QUICK MENU 헤더 복원 (온라인상담+카카오+TOP 구조)

--- components/LawQuickMenu.tsx
+++ components/LawQuickMenu.tsx
@@ -6,18 +6,26 @@ export default function LawQuickMenu() {
   return (
     <div id="quick">
       <ul>
+        {/* QUICK MENU 헤더 */}
+        <li className="quick_header">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/law/images/quick_01.png" alt="QUICK MENU" />
+        </li>
+        {/* 온라인상담 */}
         <li className="quick_menu">
           <a href="/law/mn08/consult">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/law/images/quick_02.png" alt="온라인상담" />
           </a>
         </li>
+        {/* 카카오톡 상담 */}
         <li className="quick_menu">
           <a href="https://pf.kakao.com/_uDksxb" target="_blank" rel="noopener noreferrer">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src="/law/images/quick_04.png" alt="카카오톡 상담" />
           </a>
         </li>
+        {/* TOP */}
         <li className="quick_top">
           <a href="#" onClick={(e) => { e.preventDefault(); scrollTop() }}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
